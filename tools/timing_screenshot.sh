56b8f30d6edaf9a82cf422e63fc648f21ffb5fc3
YEuler <neuyejianming@163.com>
2017-11-01 23:39:04 +0800

    截图工具

---
+++ tools/timing_screenshot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source config
+
+#curr_date=`date -d "yesterday" "+%Y%m%d_%H%M%S"` #昨天
+curr_date=`date "+%Y%m%d"` #今天
+file_name="kanban_${project_name_1}_$curr_date.jpg"
+xvfb-run CutyCapt --min-width=1366 --min-height=768 --url=$project_url_1 --out=$file_path/$file_name
+
