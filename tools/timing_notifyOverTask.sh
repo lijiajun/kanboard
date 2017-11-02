f26e408505d126785cc650235c7e0c6f1b376b3a
YEuler <neuyejianming@163.com>
2017-11-02 14:35:10 +0800

    添加通知过期任务脚本

---
+++ tools/timing_notifyOverTask.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/home/webs/kanban.aivb.pub/cli notification:overdue-tasks -p1 -g
+/home/webs/kanban.aivb.pub/cli notification:overdue-tasks -p1 -m
+
--- tools/timing_screenshot.sh
+++ tools/timing_screenshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source config
+source /home/webs/kanban.aivb.pub/tools/config
 
 #curr_date=`date -d "yesterday" "+%Y%m%d_%H%M%S"` #昨天
 curr_date=`date "+%Y%m%d"` #今天
