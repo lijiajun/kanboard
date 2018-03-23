67b04bd47a2f08c899ece464aa6ba3fd72709858
yeuler <364926865@qq.com>
2018-03-23 21:25:29 +0800

    1.修改定期截图脚本 2.添加定期发送评估通知脚本

---
+++ tools/timing_evaluateScore.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/webs/asiainfo/kanban.aivb.pub/cli notification:evaluate-tasks -s1
+/home/webs/asiainfo/kanban.aivb.pub/cli notification:evaluate-tasks -s15
--- tools/timing_screenshot.sh
+++ tools/timing_screenshot.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-source /home/webs/kanban.aivb.pub/tools/config
-
 #curr_date=`date -d "yesterday" "+%Y%m%d_%H%M%S"` #昨天
 curr_date=`date "+%Y%m%d"` #今天
-kanban_file_name="kanban_${project_name_1}_$curr_date.jpg"
-burntask_file_name="kanban_burntask_${project_name_1}_$curr_date.jpg"
-burnscore_file_name="kanban_burnscore_${project_name_1}_$curr_date.jpg"
-xvfb-run CutyCapt --min-width=1366 --min-height=768 --url=$project_url_1 --out=$file_path/$kanban_file_name
+
+if [ -z "$1" ]; then
+    echo "请输入项目编号"
+    exit 1
+fi
+
+token=`mysql -uroot -Dkanboard -e"select token from projects where id = $1;" | sed -n '2p'`
+if [ -z "$token" ]; then
+    echo "项目编号$1对应的项目不存在或未公开，请输入其他项目编号。"
+    exit 1
+fi
+
+project_name=`mysql -uroot -Dkanboard -e"select name from projects where id = $1;" | sed -n '2p'`
+if [ -z "$project_name" ]; then
+    echo "项目编号$1对应的项目不存在，请输入其他项目编号。"
+    exit 1
+fi
+
+file_path='/home/webs/kanban.aivb.pub/tools/Screenshots'
+kanban_file_name="${project_name}_kanboard_$curr_date.jpg"
+burntask_file_name="${project_name}_kanboard_${curr_date}_burntask.jpg"
+burnscore_file_name="${project_name}_kanboard_${curr_date}_burnscore.jpg"
+project_url="http://kanban.aivb.pub/?controller=BoardViewController&action=readonly&token=$token"
+project_burntasks_url="http://kanban.aivb.pub/?controller=AnalyticController&action=burndown_tasks_readonly&token=$token"
+project_burnscore_url="http://kanban.aivb.pub/?controller=AnalyticController&action=burndown_score_readonly&token=$token"
+
+xvfb-run CutyCapt --min-width=1366 --min-height=768 --url=$project_url --out=$file_path/$kanban_file_name
 sleep 3
-xvfb-run CutyCapt --min-width=1366 --min-height=400 --url=$project_burntasks_url_1 --out=$file_path/$burntask_file_name --delay=5000
+xvfb-run CutyCapt --min-width=1366 --min-height=400 --url=$project_burntasks_url --out=$file_path/$burntask_file_name --delay=5000
 sleep 3
-xvfb-run CutyCapt --min-width=1366 --min-height=400 --url=$project_burnscore_url_1 --out=$file_path/$burnscore_file_name --delay=5000
+xvfb-run CutyCapt --min-width=1366 --min-height=400 --url=$project_burnscore_url --out=$file_path/$burnscore_file_name --delay=5000
 
