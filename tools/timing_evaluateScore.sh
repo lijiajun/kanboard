93de60fe6a26275dd521b0ba148a03b039ae9632
叶建明 <yejm3@asiainfo.com>
2019-04-02 11:14:05 +0800

    更新截图脚本

--- tools/timing_evaluateScore.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/home/webs/asiainfo/kanban.aivb.pub/cli notification:evaluate-tasks -s1
-/home/webs/asiainfo/kanban.aivb.pub/cli notification:evaluate-tasks -s15
--- tools/timing_screenshot.sh
+++ tools/timing_screenshot.sh
@@ -8,13 +8,13 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-token=`mysql -uroot -Dkanboard -e"select token from projects where id = $1;" | sed -n '2p'`
+token=`mysql -uroot -Dkanboard -pmysql -e"select token from projects where id = $1;" | sed -n '2p'`
 if [ -z "$token" ]; then
     echo "项目编号$1对应的项目不存在或未公开，请输入其他项目编号。"
     exit 1
 fi
 
-project_name=`mysql -uroot -Dkanboard -e"select name from projects where id = $1;" | sed -n '2p'`
+project_name=`mysql -uroot -Dkanboard -pmysql -e"select name from projects where id = $1;" | sed -n '2p'`
 if [ -z "$project_name" ]; then
     echo "项目编号$1对应的项目不存在，请输入其他项目编号。"
     exit 1
@@ -22,15 +22,21 @@ fi
 
 file_path='/home/webs/kanban.aivb.pub/tools/Screenshots'
 kanban_file_name="${project_name}_kanboard_$curr_date.jpg"
-burntask_file_name="${project_name}_kanboard_${curr_date}_burntask.jpg"
-burnscore_file_name="${project_name}_kanboard_${curr_date}_burnscore.jpg"
+#burntask_file_name="${project_name}_kanboard_${curr_date}_burntask.jpg"
+#burnscore_file_name="${project_name}_kanboard_${curr_date}_burnscore.jpg"
 project_url="http://kanban.aivb.pub/?controller=BoardViewController&action=readonly&token=$token"
-project_burntasks_url="http://kanban.aivb.pub/?controller=AnalyticController&action=burndown_tasks_readonly&token=$token"
-project_burnscore_url="http://kanban.aivb.pub/?controller=AnalyticController&action=burndown_score_readonly&token=$token"
+#project_burntasks_url="http://kanban.aivb.pub/?controller=AnalyticController&action=burndown_tasks_readonly&token=$token"
+#project_burnscore_url="http://kanban.aivb.pub/?controller=AnalyticController&action=burndown_score_readonly&token=$token"
 
+echo $kanban_file_name
+#echo $burntask_file_name
+#echo $burnscore_file_name
+echo $project_url
+#echo $project_burntasks_url
+#echo $project_burnscore_url
 xvfb-run CutyCapt --min-width=1366 --min-height=768 --url=$project_url --out=$file_path/$kanban_file_name
-sleep 3
-xvfb-run CutyCapt --min-width=1366 --min-height=400 --url=$project_burntasks_url --out=$file_path/$burntask_file_name --delay=5000
-sleep 3
-xvfb-run CutyCapt --min-width=1366 --min-height=400 --url=$project_burnscore_url --out=$file_path/$burnscore_file_name --delay=5000
+#sleep 3
+#xvfb-run CutyCapt --min-width=1366 --min-height=400 --url=$project_burntasks_url --out=$file_path/$burntask_file_name --delay=5000
+#sleep 3
+#xvfb-run CutyCapt --min-width=1366 --min-height=400 --url=$project_burnscore_url --out=$file_path/$burnscore_file_name --delay=5000
 
